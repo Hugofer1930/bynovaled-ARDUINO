b59251b3a32b7334f23f98e79f476427e5bd7c51
Hugofer1930 <81204750+Hugofer1930@users.noreply.github.com>
2021-04-29 22:43:54 -0500

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,120 @@
+//https://www.eclipse.org/paho/clients/js/
+
+
+
+
+
+
+function Clean(){	
+	//alert("led off");
+	console.log("borrando historial");
+	message = new Paho.MQTT.Message(" ");
+    	message.destinationName = "hfcasanova.fie@unach.edu.ec/claseprueba";
+    	client.send(message);
+	//document.getElementById("sensor").innerHTML="led off";
+}
+
+	function Histo1(){	
+	//alert("led off");
+	console.log("Primer historial");
+	message = new Paho.MQTT.Message("Imprimiendo Primer historial, espere un momento");
+    	message.destinationName = "hfcasanova.fie@unach.edu.ec/claseprueba";
+    	client.send(message);
+	//document.getElementById("sensor").innerHTML="led off";
+}
+	function Histo2(){	
+	//alert("led off");
+	console.log("Segundo historial");
+	message = new Paho.MQTT.Message("Imprimiendo Segundo historial, espere un momento");
+    	message.destinationName = "hfcasanova.fie@unach.edu.ec/claseprueba";
+    	client.send(message);
+	//document.getElementById("sensor").innerHTML="led off";
+}
+
+
+
+
+
+
+
+// Create a client instance
+  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
+  
+  client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  var options = {
+   useSSL: false,
+    userName: "hfcasanova.fie@unach.edu.ec",
+    password: "1930locohUgo1996",
+    onSuccess:onConnect,
+    onFailure:doFail
+  }
+
+  // connect the client
+  client.connect(options);
+   
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("Conectado...");
+	
+    client.subscribe("hfcasanova.fie@unach.edu.ec/claseprueba");
+    message = new Paho.MQTT.Message("hola desde la web");
+    message.destinationName = "hfcasanova.fie@unach.edu.ec/claseprueba";
+    client.send(message);
+	
+  }
+
+  function doFail(e){
+    console.log(e);
+	
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+	  u=message.payloadString;
+	  
+	  
+	  //var x = document.getElementById("myText").value;
+	  if (u=="Imprimiendo historial 1, espere un momento")
+	  {
+	console.log("informacion relevante");	
+	document.getElementById("historial").innerHTML=message.payloadString;
+	  }
+	  else 
+		  if (u=="Imprimiendo historial 2, espere un momento")
+		  {
+			console.log("informacion relevante");
+			document.getElementById("historial").innerHTML=message.payloadString;
+		  }
+	 
+	  else 
+		  if (u=="hola desde la web")
+		{ 
+		console.log("informacion relevante");	 
+	 // document.getElementById("historial").innerHTML=message.payloadString;
+		}
+	  
+	  
+	  
+	  
+	 	  else {
+	  document.getElementById("historial").innerHTML=message.payloadString;
+	  }
+	  
+  }
+
+
+
+  
